94ca66c3cffa0f84e36baca8728ce090fd5f0fa4
Kate Fu <fuyuehaokate@163.com>
2022-05-11 12:19:01 +0930

    Arrays

--- src/array/RotateImage.java
+++ src/array/RotateImage.java
@@ -2,7 +2,10 @@ package array;
 
 /**
  * @ClassName RotateImage
- * @Description TODO
+ * @Description
+ *    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ *    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ *    DO NOT allocate another 2D matrix and do the rotation.
  * @Author katefu
  * @Date 5/10/22 11:45 AM
  * @Version 1.0
